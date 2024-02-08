beba92ca900e46aedb03e8cd8f305777c1bd16a7
Rafael Lima <lima.rafa68@gmail.com>
2024-02-08 13:09:57 -0300

    melhorando o carrinho, adicionando mais de um produto por pedido, add input para informações do endereço, botão de voltar ao cardápio e enviar pedido, e armazenando informações do carrinho mesmo fechando a aplicação.

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, Alert } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
+import { Feather } from "@expo/vector-icons";
 
-import { useCartStore } from "@/stores/cart-stores";
+import { ProductCartProps, useCartStore } from "@/stores/cart-stores";
 
 import { Header } from "@/components/header";
 import { Product } from "@/components/product";
 import { formatCurrency } from "@/utils/functions/format-currency";
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+import { LinkButton } from "@/components/link-button";
 
 
 export default function Cart(){
@@ -12,15 +17,32 @@ export default function Cart(){
 
     const total = formatCurrency(cartStore.products.reduce((total, product) => total + product.price * product.quantity, 0))
 
+    function handleProductRemove(product: ProductCartProps){
+        Alert.alert("Remover", `Deseja remover ${product.title} do carrinho?`,[
+            {
+                text: "Cancelar",
+            },
+            {
+                text: "Remover",
+                onPress: () => cartStore.remove(product.id),
+            },
+        ])
+    }
+
     return (
         <View className="flex-1 pt-8">
             <Header title="Seu carrinho"/>
-
-            { cartStore.products.length > 0 ? (
-
+            <KeyboardAwareScrollView>
+            <ScrollView>
             <View className="p-5 flex-1">
+            { cartStore.products.length > 0 ? (
+            <View className="border-b border-slate-700">
                 {cartStore.products.map((product) => (
-                     <Product key={product.id} data={product} />
+                     <Product 
+                     key={product.id} 
+                     data={product} 
+                     onPress={() => handleProductRemove(product)}
+                     />
                  ))}
             </View>
     ) : (
@@ -29,9 +51,26 @@ export default function Cart(){
             </Text>
             )}
 
+            
             <View className="flex-row gap-2 items-center mt-5 mb-4">
                 <Text className="text-white text-xl font-subtitle">Total:</Text>
                 <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
+            </View>
+
+            <Input placeholder="Informe o endereço de entrega com rua, bairro, CEP, número e complemento..."/>
+            </View>
+            </ScrollView>
+            </KeyboardAwareScrollView>
+
+            <View className="p-5 gap-5">
+                <Button>
+                    <Button.Text>Enviar pedido</Button.Text>
+                    <Button.Icon>
+                        <Feather name="arrow-right-circle" size={20}/>
+                    </Button.Icon>
+                </Button>
+
+                <LinkButton title="Voltar ao cardápio" href="/"/>
             </View>
         </View>
     )
